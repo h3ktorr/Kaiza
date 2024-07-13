2fbc9189799d2538ebffc40bb1e95611f0ee5a11
Kelvin Achi <kellywizy09@gmail.com>
2024-07-13 21:45:53 +0100

    fifth commit correction2

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -33,7 +33,7 @@ const ShopContextProvider = (props) => {
    const fetchData = async () => {
      try {
        const response = await axios.get(
-         "/api/products?organization_id=dc5223dc05244b6885be89f9a28790b3&Appid=R8Y1FRV0K6OATNM&Apikey=8b9e0c9e1737410b9894b1f90594690720240712145150788065"
+         "https://timbu-get-all-products.reavdev.workers.dev/?organization_id=dc5223dc05244b6885be89f9a28790b3&Appid=R8Y1FRV0K6OATNM&Apikey=8b9e0c9e1737410b9894b1f90594690720240712145150788065"
        );
        setData(response.data.items);
      } catch (error) {
