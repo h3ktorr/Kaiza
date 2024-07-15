13475d9b8f51c792c1f06cbb9adcd618bc6bdd77
Kelvin Achi <kellywizy09@gmail.com>
2024-07-15 20:54:59 +0100

    env commit

--- src/Components/SingleItem.jsx
+++ src/Components/SingleItem.jsx
@@ -7,7 +7,7 @@ const SingleItem = (props) => {
     <div className="small-img">
       <Link to={`/product/${props.id}`}>
         <img
-          src={`https://api.timbu.cloud/images/${props.imageurl}?organization_id=dc5223dc05244b6885be89f9a28790b3&Appid=R8Y1FRV0K6OATNM&Apikey=8b9e0c9e1737410b9894b1f90594690720240712145150788065`}
+          src={`https://api.timbu.cloud/images/${props.imageurl}?organization_id=dc5223dc05244b6885be89f9a28790b3&Appid=R8Y1FRV0K6OATNM&Apikey=${process.env.REACT_APP_API_KEY}`}
           alt=""
           className="small-img"
         />
